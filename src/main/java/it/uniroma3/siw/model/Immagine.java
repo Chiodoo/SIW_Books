8ff9f099d307fb493017355459478503e4e2e886
Chiodoo <marco.chiodo03@gmail.com>
2025-05-10 19:41:13 +0200

    Aggiunte Entità iniziali

---
+++ src/main/java/it/uniroma3/siw/model/Immagine.java
@@ -0,0 +1,69 @@
+package it.uniroma3.siw.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Immagine {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String path;        //Path dell'immagine nel db
+
+    @ManyToOne
+    private Libro libro;       //Libro a cui appartiene l'immagine
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((path == null) ? 0 : path.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Immagine other = (Immagine) obj;
+        if (path == null) {
+            if (other.path != null)
+                return false;
+        } else if (!path.equals(other.path))
+            return false;
+        return true;
+    }
+}
---
+++ src/main/java/it/uniroma3/siw/model/Libro.java
@@ -0,0 +1,136 @@
+package it.uniroma3.siw.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Libro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String titolo;
+
+    @Min(0)
+    @Max(2025)
+    private Long annoPubblicazione;
+
+    @Min(0)
+    private Long edizione;
+
+    @OneToMany(mappedBy = "libro", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    private List<Immagine> immagini;
+    //Non basta solo il cascade MERGE, ma serve anche l'orphanRemoval=true per eliminare le immagini quando vengono eliminate dal libro
+    //CascadeType.MERGE aggiorna solo le entità figlie esistenti
+    //orphanRemoval=true elimina le entità figlie che non sono più referenziate dal libro
+
+    @ManyToMany(mappedBy = "libri")
+    private List<Autore> autori;
+
+    @OneToMany(mappedBy = "libro", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Recensione> recensioni;
+
+    //===================================METODI=========================================
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public Long getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public void setAnnoPubblicazione(Long annoPubblicazione) {
+        this.annoPubblicazione = annoPubblicazione;
+    }
+
+    public List<Immagine> getImmagini() {
+        return immagini;
+    }
+
+    public void setImmagini(List<Immagine> immagini) {
+        this.immagini = immagini;
+    }
+
+    public List<Autore> getAutori() {
+        return autori;
+    }
+
+    public void setAutori(List<Autore> autori) {
+        this.autori = autori;
+    }
+
+    public List<Recensione> getRecensioni() {
+        return recensioni;
+    }
+
+    public void setRecensioni(List<Recensione> recensioni) {
+        this.recensioni = recensioni;
+    }
+
+    public Long getEdizione() {
+        return edizione;
+    }
+
+    public void setEdizione(Long edizione) {
+        this.edizione = edizione;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((titolo == null) ? 0 : titolo.hashCode());
+        result = prime * result + ((edizione == null) ? 0 : edizione.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Libro other = (Libro) obj;
+        if (titolo == null) {
+            if (other.titolo != null)
+                return false;
+        } else if (!titolo.equals(other.titolo))
+            return false;
+        if (edizione == null) {
+            if (other.edizione != null)
+                return false;
+        } else if (!edizione.equals(other.edizione))
+            return false;
+        return true;
+    }
+}
