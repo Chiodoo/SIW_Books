ef5aeda2fdefb033e20af187a73f0be4d05de7ef
Chiodoo <marco.chiodo03@gmail.com>
2025-06-02 12:26:08 +0200

    Implementato upload photo per user

--- src/main/java/it/uniroma3/siw/util/FileUploadUtil.java
+++ src/main/java/it/uniroma3/siw/util/FileUploadUtil.java
@@ -10,10 +10,8 @@ import java.nio.file.StandardCopyOption;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileUploadUtil {
-
-    public static void saveFile(String entityDir, String fileName, MultipartFile multipartFile) throws IOException {
-        String uploadBase = "uploads/" + entityDir;
-        Path uploadPath = Paths.get(uploadBase);
+    public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException {
+        Path uploadPath = Paths.get(uploadDir);
 
         if (!Files.exists(uploadPath)) {
             Files.createDirectories(uploadPath);
@@ -22,6 +20,8 @@ public class FileUploadUtil {
         try (InputStream inputStream = multipartFile.getInputStream()) {
             Path filePath = uploadPath.resolve(fileName);
             Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new IOException("Errore nel salvataggio file: " + fileName, e);
         }
     }
 }
