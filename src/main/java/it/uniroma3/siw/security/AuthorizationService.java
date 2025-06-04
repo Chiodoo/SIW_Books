a508a45d21142f379bd42a56202ee80d2ed86a6a
Chiodoo <marco.chiodo03@gmail.com>
2025-06-04 19:12:20 +0200

    fix minori

--- src/main/java/it/uniroma3/siw/security/AuthorizationService.java
+++ src/main/java/it/uniroma3/siw/security/AuthorizationService.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import it.uniroma3.siw.model.Credentials;
@@ -22,7 +21,7 @@ public class AuthorizationService {
      * Gli utenti OAuth2 non sono salvati nel DB e quindi sono trattati come utenti normali.
      */
     public boolean isAdmin() {
-        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null || !authentication.isAuthenticated()) {
             return false;
