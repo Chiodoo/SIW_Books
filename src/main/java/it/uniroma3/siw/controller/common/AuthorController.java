6cd174a795d5caa8a35b878e3c5e10ef1d8bf5ba
Chiodoo <marco.chiodo03@gmail.com>
2025-07-26 12:18:05 +0200

    Aggiunto component viewResolver per evitare codice duplicato tra i controller con operazioni comuni

--- src/main/java/it/uniroma3/siw/controller/common/AuthorController.java
+++ src/main/java/it/uniroma3/siw/controller/common/AuthorController.java
@@ -1,13 +1,13 @@
 package it.uniroma3.siw.controller.common;
 
-import static it.uniroma3.siw.model.Credentials.ADMIN_ROLE;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import it.uniroma3.siw.configuration.ViewResolver;
 import it.uniroma3.siw.model.Author;
 import it.uniroma3.siw.security.UserPrincipal;
 import it.uniroma3.siw.service.AuthorService;
@@ -18,10 +18,13 @@ public class AuthorController {
     @Autowired
     private AuthorService authorService;
 
+    @Autowired
+    private ViewResolver viewResolver;
+
     @GetMapping("/authors")
     public String getAuthors(Model model, @AuthenticationPrincipal UserPrincipal self) {
         model.addAttribute("authors", authorService.getAllAuthors());
-        return viewFor("authors", self);
+        return viewResolver.viewFor("authors", self);
     }
 
     @GetMapping("/author/{id}")
@@ -29,11 +32,6 @@ public class AuthorController {
         Author author = authorService.findById(id)
             .orElseThrow(() -> new IllegalArgumentException("Autore non trovato: " + id));
         model.addAttribute("author", author);
-        return viewFor("author", self);
-    }
-
-    private String viewFor(String templateBase, UserPrincipal self) {
-        boolean isAdmin = self != null && ADMIN_ROLE.equals(self.getRole());
-        return (isAdmin ? "admin/" : "") + templateBase;
+        return viewResolver.viewFor("author", self);
     }
 }
