cfa636ed5d1b5893f36bd4ac071c48d327d49be4
Chiodoo <marco.chiodo03@gmail.com>
2025-07-26 11:52:48 +0200

    Refactor BookController per evitare codice duplicato tra admin e non

--- src/main/java/it/uniroma3/siw/controller/common/BookController.java
+++ src/main/java/it/uniroma3/siw/controller/common/BookController.java
@@ -1,8 +1,13 @@
 package it.uniroma3.siw.controller.common;
 
+import static it.uniroma3.siw.model.Credentials.ADMIN_ROLE;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
+import it.uniroma3.siw.security.UserPrincipal;
 import it.uniroma3.siw.service.BookService;
 import it.uniroma3.siw.service.RecensioneService;
 
@@ -18,15 +23,20 @@ public class BookController {
     @Autowired RecensioneService recensioneService;
 
     @GetMapping("/book/{id}")
-    public String getBook(@PathVariable("id") Long id, Model model) {
+    public String getBook(@PathVariable("id") Long id, Model model,  @AuthenticationPrincipal UserPrincipal self) {
         model.addAttribute("book", this.bookService.findById(id));
         model.addAttribute("reviews", this.recensioneService.findByBookId(id));
-        return "book";
+        return viewFor("book", self);
     }
 
     @GetMapping("/books")
-    public String getAllBooks(Model model) {
+    public String getAllBooks(Model model, @AuthenticationPrincipal UserPrincipal self) {
         model.addAttribute("books", this.bookService.getAllBooks());
-        return "books";
+        return viewFor("books", self);
+    }
+
+    private String viewFor(String templateBase, UserPrincipal self) {
+        boolean isAdmin = self != null && ADMIN_ROLE.equals(self.getRole());
+        return (isAdmin ? "admin/" : "") + templateBase;
     }
 }
