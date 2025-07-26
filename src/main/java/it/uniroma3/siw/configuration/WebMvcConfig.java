b3e6537b6d720573ee5df54ce51fa58baf9ecd50
Chiodoo <marco.chiodo03@gmail.com>
2025-07-26 19:22:49 +0200

    Aggiunta cancellazione dei libri, relative dipendenze e directory con immagini dal filesystem

--- src/main/java/it/uniroma3/siw/configuration/WebMvcConfig.java
+++ src/main/java/it/uniroma3/siw/configuration/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -18,6 +19,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Bean(name = "multipartResolver")
     public MultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
+    }
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
     }
 
     @Override
